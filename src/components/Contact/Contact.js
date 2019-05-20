5c1865ad13eff7d0b36d31bf309b1445c8dd70af
Ashylen <ashylen@o2.pl>
2019-05-20 09:38:19 +0200

    Added validation for Contact form and refractored its styles.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,18 +9,85 @@ class Contact extends React.Component {
     super(props);
 
     this.state = {
-      text: ""
+      fields: {
+        name: "",
+        email: "",
+        messsage: ""
+      },
+      errors: {
+        name: "",
+        email: "",
+        messsage: ""
+      },
+      isFormSubmitClicked: false
     };
+
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleSubmit = e => {
+  handleFormSubmit = e => {
     e.preventDefault();
-    console.log("submit");
+    this.setState({ isFormSubmitClicked: true });
+
+    if (this.validateForm()) {
+      this.handleClearForm();
+      alert("Form submitted");
+    }
   };
+
+  handleClearForm() {
+    let fields = {};
+    fields["name"] = "";
+    fields["email"] = "";
+    fields["message"] = "";
+    this.setState({ fields: fields });
+  }
 
   handleInputChange = e => {
+    let fields = this.state.fields;
+    fields[e.target.name] = e.target.value;
 
+    if(this.state.isFormSubmitClicked)
+      this.validateForm();
+
+    this.setState({
+      fields
+    });
   };
+
+  validateForm() {
+    let fields = this.state.fields;
+    let errors = {};
+    let isFormValid = true;
+
+    if (!fields["name"]) {
+      isFormValid = false;
+      errors["name"] = "This field is required";
+    }
+
+
+    if (typeof fields["email"] !== "undefined") {
+      var pattern = new RegExp(
+        /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+      );
+      if (!pattern.test(fields["email"])) {
+        isFormValid = false;
+        errors["email"] = "Please enter valid Email address";
+      }
+    }
+
+     if (!fields["email"]) {
+       isFormValid = false;
+       errors["email"] = "This field is required";
+     }
+
+
+    this.setState({
+      errors: errors
+    });
+    return isFormValid;
+  }
 
   render() {
     return (
@@ -28,23 +95,28 @@ class Contact extends React.Component {
         <div className={styles.wrapper}>
           <div className={styles.formWrapper}>
             <Notification />
-            <form className={styles.form} onSubmit={this.handleSubmit}>
+            <form className={styles.form} onSubmit={this.handleFormSubmit} noValidate>
               <Input
                 name="name"
                 label="Name"
-                required="true"
-                onChange={this.handleInputChange}
+                value={this.state.fields.name}
+                required={false}
+                onChangeFn={this.handleInputChange}
+                errorMsg={this.state.errors.name}
               />
               <Input
                 name="email"
                 label="Email"
-                required="true"
-                onChange={this.handleInputChange}
+                value={this.state.fields.email}
+                required={false}
+                onChangeFn={this.handleInputChange}
+                errorMsg={this.state.errors.email}
               />
               <Input
                 name="message"
                 label="Message"
-                onChange={this.handleInputChange}
+                value={this.state.fields.message}
+                onChangeFn={this.handleInputChange}
               />
               <div className={styles.buttonWrapper}>
                 <Button>Send</Button>
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,32 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Input.module.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Input.module.scss";
+import classNames from "classnames";
 
-const Input = ({ tag: Tag, name, label, required }) => (
-  <div className={styles.formItem}>
+const Input = ({
+  tag: Tag,
+  name,
+  label,
+  value,
+  required,
+  onChangeFn,
+  errorMsg
+}) => (
+  <div
+    className={classNames({
+      [styles.formItem]: true,
+      [styles.formItemInvalid]: errorMsg ? true : false
+    })}
+  >
     <Tag
-      className={styles.input}
+      className={styles.formItemInput}
       type="text"
       name={name}
       id={name}
       placeholder=" "
+      value={value}
       required={required ? 1 : 0}
+      onChange={onChangeFn}
     />
-    <label className={styles.label} htmlFor={name}>
-      {label}{required ? [<sup key={name} className={styles.required}>*</sup>] : ""}
+    <label className={styles.formItemLabel} htmlFor={name}>
+      {label}
+      {required
+        ? [
+            <sup key={name} className={styles.required}>
+              *
+            </sup>
+          ]
+        : ""}
     </label>
     <div className={styles.formItemBar} />
+    {errorMsg ? <div className={styles.errorMsg}>{errorMsg}</div> : ""}
   </div>
 );
 
 Input.propTypes = {
-    name: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-}
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
+};
 
 Input.defaultProps = {
-    tag: 'input'
-}
+  tag: "input"
+};
 
 export default Input;
